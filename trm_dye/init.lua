fe5e08b6d967db01d9a1ae403e954426e2947dbf
Jacob Gustafson <7557867+expertmm@users.noreply.github.com>
2018-02-08 13:01:04 -0500

    changed dye list for latest stable minetest_game

--- trm_dye/init.lua
+++ trm_dye/init.lua
@@ -1,4 +1,4 @@
-local colors = {"white", "lightgrey", "grey", "darkgrey", "black", "red", "orange", "yellow", "lime", "green", "aqua", "cyan", "sky_blue", "blue", "violet", "magenta", "red_violet"}
+local colors = {"white", "grey", "dark_grey", "black", "violet", "blue", "cyan", "dark_green", "green", "yellow", "brown", "orange", "red", "magenta", "pink"}
 for i=1,#colors do
 	treasurer.register_treasure("dye:"..colors[i], 0.0117, 1, {1,6}, nil, "crafting_component" )
 end
